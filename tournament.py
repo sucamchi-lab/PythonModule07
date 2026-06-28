c8e06dd18b72b68eb00859fe0dc1d343456afcfc
sucamchi-lab <sucamchi@gmail.com>
2026-06-28 16:52:08 +0200

    finish tournament.py

--- tournament.py
+++ tournament.py
@@ -1,2 +1,58 @@
 from ex0 import CreatureFactory, AquaFactory, FlameFactory
 from ex1 import HealingCreatureFactory, TransformCreatureFactory
+from ex2 import (
+    AggressiveStrategy,
+    BattleStrategy,
+    DefensiveStrategy,
+    NormalStrategy,
+)
+
+
+def battle(opponents: list[tuple[CreatureFactory, BattleStrategy]]) -> None:
+    print(f"{len(opponents)} opponents involved")
+    for i in range(len(opponents)):
+        for j in range(i + 1, len(opponents)):
+            factory1, strategy1 = opponents[i]
+            factory2, strategy2 = opponents[j]
+            creature1 = factory1.create_base()
+            creature2 = factory2.create_base()
+            print()
+            print("* Battle *")
+            print(creature1.describe())
+            print("vs.")
+            print(creature2.describe())
+            print("now fight!")
+            try:
+                strategy1.act(creature1)
+                strategy2.act(creature2)
+            except Exception as e:
+                print(e)
+
+
+if __name__ == "__main__":
+    print("Tournament 0 (basic)")
+    print("[ (Flameling+Normal), (Healing+Defensive) ]")
+    print("*** Tournament ***")
+    battle([
+        (FlameFactory(), NormalStrategy()),
+        (HealingCreatureFactory(), DefensiveStrategy()),
+    ])
+    print()
+
+    print("Tournament 1 (error)")
+    print("[ (Flameling+Aggressive), (Healing+Defensive) ]")
+    print("*** Tournament ***")
+    battle([
+        (FlameFactory(), AggressiveStrategy()),
+        (HealingCreatureFactory(), DefensiveStrategy()),
+    ])
+    print()
+
+    print("Tournament 2 (multiple)")
+    print("[ (Aquabub+Normal), (Healing+Defensive), (Transform+Aggressive) ]")
+    print("*** Tournament ***")
+    battle([
+        (AquaFactory(), NormalStrategy()),
+        (HealingCreatureFactory(), DefensiveStrategy()),
+        (TransformCreatureFactory(), AggressiveStrategy()),
+    ])
